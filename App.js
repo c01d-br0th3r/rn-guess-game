30d1f6d7b6536fb4f7d6eb6d160505a22ce126b8
c01d-br0th3r <60615552+c01d-br0th3r@users.noreply.github.com>
2021-03-31 02:49:15 +0900

    feat(App.js): use App.js file as Container

--- App.js
+++ App.js
@@ -1,13 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
+
 import Header from "./components/Header";
-import StartGameScreen from "./pages/StartGameScreen";
+import StartGameScreen from "./screens/StartGameScreen";
+import GameScreen from "./screens/GameScreen";
+import GameOverScreen from "./screens/GameOverScreen";
 
 export default function App() {
+  const [userNumber, setUserNumber] = useState();
+  const [guessRounds, setGuessRounds] = useState(0);
+
+  const handleNewGame = () => {
+    setGuessRounds(0);
+    setUserNumber(null);
+  };
+
+  const handleStartGame = (selectedNumber) => {
+    setUserNumber(selectedNumber);
+  };
+
+  const handleGameOver = (numOfRounds) => {
+    setGuessRounds(numOfRounds);
+  };
+
+  let content = <StartGameScreen onStartGame={handleStartGame} />;
+
+  if (userNumber && guessRounds <= 0) {
+    content = (
+      <GameScreen userChoice={userNumber} onGameOver={handleGameOver} />
+    );
+  } else if (guessRounds > 0) {
+    content = (
+      <GameOverScreen
+        roundsNumber={guessRounds}
+        userNumber={userNumber}
+        onRestart={handleNewGame}
+      />
+    );
+  }
+
   return (
     <View style={styles.screen}>
       <Header title="Guess A Number" />
-      <StartGameScreen />
+      {content}
     </View>
   );
 }
